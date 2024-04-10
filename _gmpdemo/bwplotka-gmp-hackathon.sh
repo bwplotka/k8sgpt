9736156fe1a6fab622c8a836bdc967128255a0c4
bwplotka <bwplotka@gmail.com>
2024-04-10 10:30:59 +0100

    Demo cluster.
    Signed-off-by: bwplotka <bwplotka@gmail.com>

--- _gmpdemo/bwplotka-gmp-hackathon.sh
+++ _gmpdemo/bwplotka-gmp-hackathon.sh
@@ -17,7 +17,10 @@ function k8sgpt() {
     ${DIR}/../bin/k8sgpt "$@"
 }
 
+
 clear
+
+export CLUSTER_NAME="bwplotka-stdtest"
 
 printf "Let's imagine our customers creates a new namespace called 'demo' in their GKE cluster with GMP Collection enabled.\n\nThey've created a 'demo' namespace and plan to deploy & monitor a new deployment:\n\n"
 
@@ -27,7 +30,7 @@ r "kubectl get -n demo po"
 
 r "kubectl apply -n demo -f manifests/my-application-pod-monitoring1.yaml" "kubectl apply -n demo -f manifests/pm-wrong-selector.yaml"
 r "cat manifests/my-application-pod-monitoring1.yaml" "cat manifests/pm-wrong-selector.yaml"
-r "Checking GCM for heap memory metric for 'my-application' pods... Nothing there!" "open 'https://pantheon.corp.google.com/monitoring/metrics-explorer;duration=PT5M?project=gpe-test-1&pageState=%7B%22xyChart%22:%7B%22constantLines%22:%5B%5D,%22dataSets%22:%5B%7B%22plotType%22:%22LINE%22,%22prometheusQuery%22:%22go_memstats_alloc_bytes%7Bcluster%3D%5C%22bwplotka-managed-test%5C%22,%20pod%3D~%5C%22my-application.*%5C%22%7D%22,%22targetAxis%22:%22Y1%22,%22unitOverride%22:%22%22%7D%5D,%22options%22:%7B%22mode%22:%22COLOR%22%7D,%22y1Axis%22:%7B%22label%22:%22%22,%22scale%22:%22LINEAR%22%7D%7D%7D'"
+r "Checking GCM for heap memory metric for 'my-application' pods... Nothing there!" "open 'https://pantheon.corp.google.com/monitoring/metrics-explorer;duration=PT5M?project=gpe-test-1&pageState=%7B%22xyChart%22:%7B%22constantLines%22:%5B%5D,%22dataSets%22:%5B%7B%22plotType%22:%22LINE%22,%22prometheusQuery%22:%22go_memstats_alloc_bytes%7Bcluster%3D%5C%22${CLUSTER_NAME}%5C%22,%20pod%3D~%5C%22my-application.*%5C%22%7D%22,%22targetAxis%22:%22Y1%22,%22unitOverride%22:%22%22%7D%5D,%22options%22:%7B%22mode%22:%22COLOR%22%7D,%22y1Axis%22:%7B%22label%22:%22%22,%22scale%22:%22LINEAR%22%7D%7D%7D'"
 
 r "Let's analyze GMP PodMonitoring using K8sGPT tool! 🚀" "open 'https://k8sgpt.ai/'"
 
@@ -56,7 +59,7 @@ r "kubectl apply -n demo -f manifests/my-application-pod-monitoring-fixed.yaml" 
 r "cat manifests/my-application-pod-monitoring-fixed.yaml" "cat manifests/pm-correct.yaml"
 
 r "k8sgpt analyze -c -n demo -f PodMonitoring --explain # Should be fine?"
-r "We should have now our heap memory metric for 'my-application' pod in GCM! 💪🏽" "open 'https://pantheon.corp.google.com/monitoring/metrics-explorer;duration=PT5M?project=gpe-test-1&pageState=%7B%22xyChart%22:%7B%22constantLines%22:%5B%5D,%22dataSets%22:%5B%7B%22plotType%22:%22LINE%22,%22prometheusQuery%22:%22go_memstats_alloc_bytes%7Bcluster%3D%5C%22bwplotka-managed-test%5C%22,%20pod%3D~%5C%22my-application.*%5C%22%7D%22,%22targetAxis%22:%22Y1%22,%22unitOverride%22:%22%22%7D%5D,%22options%22:%7B%22mode%22:%22COLOR%22%7D,%22y1Axis%22:%7B%22label%22:%22%22,%22scale%22:%22LINEAR%22%7D%7D%7D'"
+r "We should have now our heap memory metric for 'my-application' pod in GCM! 💪🏽" "open 'https://pantheon.corp.google.com/monitoring/metrics-explorer;duration=PT5M?project=gpe-test-1&pageState=%7B%22xyChart%22:%7B%22constantLines%22:%5B%5D,%22dataSets%22:%5B%7B%22plotType%22:%22LINE%22,%22prometheusQuery%22:%22go_memstats_alloc_bytes%7Bcluster%3D%5C%22${CLUSTER_NAME}%5C%22,%20pod%3D~%5C%22my-application.*%5C%22%7D%22,%22targetAxis%22:%22Y1%22,%22unitOverride%22:%22%22%7D%5D,%22options%22:%7B%22mode%22:%22COLOR%22%7D,%22y1Axis%22:%7B%22label%22:%22%22,%22scale%22:%22LINEAR%22%7D%7D%7D'"
 
 r "Next steps: Discuss with K8sGPT how to integrate that and give to OSS users; Perhaps add as a GMP extension, our own CLI or UI feature?" "open 'https://github.com/k8sgpt-ai/k8sgpt/pull/857'"
 r "That's it, thanks!" "echo '🤙🏽'"
